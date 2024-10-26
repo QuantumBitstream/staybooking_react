bc25e50934dd5093c84668b0d41b3d27d9bd6c0e
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-10-27 05:24:24 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout,Dropdown, Menu,Button } from "antd";
+import {Useroutlined } from "@ant-design/icons";
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Header,Content } = Layout;
+
+class App extends React.Component {
+  render() {
+    return (
+        <Layout style={{ height: "100vh" }}>
+          <Header></Header>
+          <Content></Content>
+        </Layout>
+    );
+  }
 }
 
 export default App;
+
