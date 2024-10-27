f9692b6a0cad1ed9643741a1bc65ef0c1045df4c
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-10-28 07:11:57 +0800

    Update HostHpmePage.js

--- src/components/HostHpmePage.js
+++ src/components/HostHpmePage.js
@@ -16,12 +16,51 @@ export class StayDetailInfoButton extends React.Component {
 }
 
 
+class MyStays extends React.Component {
+    state = {
+        loading: false,
+        data: [],
+    };
+
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+
+    loadData = async () => {
+        this.setState({
+            loading: true,
+        });
+
+
+        try {
+            const resp = await getStaysByHost();
+            this.setState({
+                data: resp,
+            });
+        } catch (error) {
+            message.error(error.message);
+        } finally {
+            this.setState({
+                loading: false,
+            });
+        }
+    };
+
+
+    render() {
+        return <></>;
+    }
+}
+
+
 class HostHomePage extends React.Component {
     render() {
         return (
             <Tabs defaultActiveKey="1" destroyInactiveTabPane={true}>
                 <TabPane key="1" tab="My Stays">
-                    <div>My Stays Content</div>
+                    <MyStays />
                 </TabPane>
                 <TabPane key="2" tab="Upload Stay">
                     <UploadStay />
