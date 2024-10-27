9a16fb28907c0386acd95ab487715d982d78ee48
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-10-28 07:12:37 +0800

    Update HostHpmePage.js

--- src/components/HostHpmePage.js
+++ src/components/HostHpmePage.js
@@ -50,7 +50,51 @@ class MyStays extends React.Component {
 
 
     render() {
-        return <></>;
+        return (
+            <List
+                loading={this.state.loading}
+                grid={{
+                    gutter: 16,
+                    xs: 1,
+                    sm: 3,
+                    md: 3,
+                    lg: 3,
+                    xl: 4,
+                    xxl: 4,
+                }}
+                dataSource={this.state.data}
+                renderItem={(item) => (
+                    <List.Item>
+                        <Card
+                            key={item.id}
+                            title={
+                                <div style={{ display: "flex", alignItems: "center" }}>
+                                    <Text ellipsis={true} style={{ maxWidth: 150 }}>
+                                        {item.name}
+                                    </Text>
+                                    <StayDetailInfoButton stay={item} />
+                                </div>
+                            }
+                            actions={[]}
+                            extra={null}
+                        >
+                            <Carousel
+                                dots={false}
+                                arrows={true}
+                                prevArrow={<LeftCircleFilled />}
+                                nextArrow={<RightCircleFilled />}
+                            >
+                                {item.images.map((image, index) => (
+                                    <div key={index}>
+                                        <Image src={image} width="100%" />
+                                    </div>
+                                ))}
+                            </Carousel>
+                        </Card>
+                    </List.Item>
+                )}
+            />
+        );
     }
 }
 
