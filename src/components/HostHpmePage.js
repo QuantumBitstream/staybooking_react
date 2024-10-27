1e014ae257e7a59db83e6bb329906ed4331c71d3
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-10-28 05:59:19 +0800

    Update HostHpmePage.js

--- src/components/HostHpmePage.js
+++ src/components/HostHpmePage.js
@@ -1,5 +1,6 @@
 import { Tabs } from "antd";
 import React from "react";
+import UploadStay from "./UploadStay";
 
 
 const { TabPane } = Tabs;
@@ -13,7 +14,8 @@ class HostHomePage extends React.Component {
                     <div>My Stays Content</div>
                 </TabPane>
                 <TabPane key="2" tab="Upload Stay">
-                    <div>Upload Stays</div>
+                    {/*<div>Upload Stays</div>*/}
+                    <UploadStay />
                 </TabPane>
             </Tabs>
         );
