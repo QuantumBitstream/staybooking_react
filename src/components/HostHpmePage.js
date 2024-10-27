9f136d0bc5ca612142896965d404c569a58c9e3d
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-10-28 06:30:01 +0800

    Update HostHpmePage.js

--- src/components/HostHpmePage.js
+++ src/components/HostHpmePage.js
@@ -1,9 +1,19 @@
-import { Tabs } from "antd";
+import { message, Tabs, List, Card, Image, Carousel } from "antd";
+import { LeftCircleFilled, RightCircleFilled } from "@ant-design/icons";
+import Text from "antd/lib/typography/Text";
 import React from "react";
+import { getStaysByHost } from "../utils";
 import UploadStay from "./UploadStay";
 
 
 const { TabPane } = Tabs;
+
+
+export class StayDetailInfoButton extends React.Component {
+    render() {
+        return <></>;
+    }
+}
 
 
 class HostHomePage extends React.Component {
@@ -14,15 +24,12 @@ class HostHomePage extends React.Component {
                     <div>My Stays Content</div>
                 </TabPane>
                 <TabPane key="2" tab="Upload Stay">
-                    {/*<div>Upload Stays</div>*/}
                     <UploadStay />
                 </TabPane>
             </Tabs>
         );
     }
 }
-
-
 export default HostHomePage;
 
 
