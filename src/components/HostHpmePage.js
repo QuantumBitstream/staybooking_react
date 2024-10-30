4c98d3630caadab8e0dc201a3c14f62b984fd6f3
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-10-30 10:42:16 +0800

    Update HostHpmePage.js

--- src/components/HostHpmePage.js
+++ src/components/HostHpmePage.js
@@ -1,5 +1,20 @@
-import { message, Tabs, List, Card, Image, Carousel } from "antd";
-import { LeftCircleFilled, RightCircleFilled } from "@ant-design/icons";
+import {
+    message,
+    Tabs,
+    List,
+    Card,
+    Image,
+    Carousel,
+    Tooltip,
+    Button,
+    Modal,
+    Space,
+} from "antd";
+import {
+    LeftCircleFilled,
+    RightCircleFilled,
+    InfoCircleOutlined,
+} from "@ant-design/icons";
 import Text from "antd/lib/typography/Text";
 import React from "react";
 import { getStaysByHost } from "../utils";
@@ -10,8 +25,62 @@ const { TabPane } = Tabs;
 
 
 export class StayDetailInfoButton extends React.Component {
+    state = {
+        modalVisible: false,
+    };
+
+
+    openModal = () => {
+        this.setState({
+            modalVisible: true,
+        });
+    };
+
+
+    handleCancel = () => {
+        this.setState({
+            modalVisible: false,
+        });
+    };
+
+
     render() {
-        return <></>;
+        const { stay } = this.props;
+        const { name, description, address, guestNumber } = stay;
+        const { modalVisible } = this.state;
+
+
+        return (
+            <>
+                <Tooltip title="View Stay Details">
+                    <Button
+                        onClick={this.openModal}
+                        style={{ border: "none" }}
+                        size="large"
+                        icon={<InfoCircleOutlined />}
+                    />
+                </Tooltip>
+                {modalVisible && (
+                    <Modal
+                        title={name}
+                        centered={true}
+                        visible={modalVisible}
+                        closable={false}
+                        footer={null}
+                        onCancel={this.handleCancel}
+                    >
+                        <Space direction="vertical">
+                            <Text strong={true}>Description</Text>
+                            <Text type="secondary">{description}</Text>
+                            <Text strong={true}>Address</Text>
+                            <Text type="secondary">{address}</Text>
+                            <Text strong={true}>Guest Number</Text>
+                            <Text type="secondary">{guestNumber}</Text>
+                        </Space>
+                    </Modal>
+                )}
+            </>
+        );
     }
 }
 
