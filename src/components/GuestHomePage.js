4eebcbe1d54d091db174152b06d26beca0bc2e1d
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-11-01 10:27:00 +0800

    Update GuestHomePage.js

--- src/components/GuestHomePage.js
+++ src/components/GuestHomePage.js
@@ -1,19 +1,61 @@
-import {
-    Tabs
-} from "antd";
-
 import React from "react";
-
+import { Form, List, message, Tabs } from "antd";
+import { searchStays } from "../utils";
 
 
 const { TabPane } = Tabs;
+
+
+class SearchStays extends React.Component {
+    state = {
+        data: [],
+        loading: false,
+    };
+
+
+    search = async (query) => {
+        this.setState({
+            loading: true,
+        });
+
+
+        try {
+            const resp = await searchStays(query);
+            this.setState({
+                data: resp,
+            });
+        } catch (error) {
+            message.error(error.message);
+        } finally {
+            this.setState({
+                loading: false,
+            });
+        }
+    };
+
+
+    render() {
+        return (
+            <>
+                <Form onFinish={this.search} layout="inline">
+                    <Form.Item></Form.Item>
+                    <Form.Item></Form.Item>
+                    <Form.Item></Form.Item>
+                    <Form.Item></Form.Item>
+                </Form>
+                <List></List>
+            </>
+        );
+    }
+}
+
 
 class GuestHomePage extends React.Component {
     render() {
         return (
             <Tabs defaultActiveKey="1" destroyInactiveTabPane={true}>
                 <TabPane key="1" tab="Search Stays">
-                    Search Stays Content
+                    <SearchStays />
                 </TabPane>
                 <TabPane key="2" tab="My Reservations">
                     My Reservations Content
@@ -25,3 +67,5 @@ class GuestHomePage extends React.Component {
 
 
 export default GuestHomePage;
+
+
